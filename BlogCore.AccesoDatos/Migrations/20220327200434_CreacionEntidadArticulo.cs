647f577b612cfac82a4a02070f1d81dad2a125ca
DESKTOP-311EKJF\andrew <ramirezandrew172@gmail.com>
2022-03-27 19:47:10 -0500

    Initial Commit

---
+++ BlogCore.AccesoDatos/Migrations/20220327200434_CreacionEntidadArticulo.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlogCore.AccesoDatos.Migrations
+{
+    public partial class CreacionEntidadArticulo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Precio",
+                table: "Articulo",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Proveedor",
+                table: "Articulo",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Stock",
+                table: "Articulo",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Precio",
+                table: "Articulo");
+
+            migrationBuilder.DropColumn(
+                name: "Proveedor",
+                table: "Articulo");
+
+            migrationBuilder.DropColumn(
+                name: "Stock",
+                table: "Articulo");
+        }
+    }
+}
---
+++ BlogCore.Models/Articulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BlogCore.Models
+{
+    public class Articulo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [Display(Name = "Nombre del artículo")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El precio es obligatoria")]
+        public float Precio { get; set; }
+
+        [Required(ErrorMessage = "El stock es obligatoria")]
+        public int Stock { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria")]       
+        public string Descripcion { get; set; }
+
+        [Display(Name = "Fecha de creación")]
+        public string FechaCreacion { get; set; }
+
+        [Display(Name = "Fecha de creación")]
+        public string Proveedor { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        [Display(Name = "Imagen")]
+        public string UrlImagen { get; set; }
+
+        [Required]
+        public int CategoriaId { get; set; }
+
+        [ForeignKey("CategoriaId")]
+        public Categoria Categoria { get; set; }
+
+    }
+}
